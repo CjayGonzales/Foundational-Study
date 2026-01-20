06ae34364c15fb77e216db7a870a25b88f8d76ab
Clemente Gonzales <cjaygonzales117@gmail.com>
2026-01-20 20:14:54 +0000

    attempted to solve box puzzle using recursion

--- grokking_algorithms/recursion.py
+++ grokking_algorithms/recursion.py
@@ -15,7 +15,31 @@ def till_zero(item):
    else:
     till_zero(item -1)
 
-till_zero(20)
+# till_zero(20)
 
+# call stack factorial
 
+def find_fact(x):
+    if x == 1:
+      return 1
+    else:
+      mult = x * find_fact(x-1)
+      print(mult)
+
+      return x * find_fact(x-1)
+    
+# find_fact(3)
       
+
+
+def key_finder(box):
+   x=0
+   if box == "key":
+      print("Found key!")
+      return 
+   else:
+      key_finder(box[-1])
+      print(x)
+      x += 1
+
+key_finder(["box","box","box","box","box","box","key"])
